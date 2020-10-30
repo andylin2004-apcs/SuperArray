0f3464e02d7a8be7fdc9427709798af1d0eb8652
Andy Lin <alin21@stuy.edu>
2020-10-30 15:41:56 -0400

    added add()

--- SuperArray.java
+++ SuperArray.java
@@ -10,5 +10,12 @@ class SuperArray{
   public int size(){
     return size;
   }
-  
+
+  public boolean add(String element){
+    for (int i = 0; i<size; i++){
+      if(data[i] == nil){
+        data[i] == element;
+      }
+    }
+  }
 }
