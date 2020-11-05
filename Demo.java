96149b22645b4fc66fd598ba7cc8c83e313230e4
Andy Lin <alin21@stuy.edu>
2020-11-05 16:09:19 -0500

    added zip

--- Demo.java
+++ Demo.java
@@ -21,8 +21,20 @@ public class Demo{
     removeDuplicates(result);
     return result;
   }
-  /**/public static void main(String[]args){
-    /*SuperArray words = new SuperArray();
+  public static SuperArray zip(SuperArray a, SuperArray b){
+    SuperArray result = new SuperArray();
+    for (int i = 0; i<Math.min(a.size(),b.size()); i++){
+      if (i < a.size()){
+        result.add(a.get(i));
+      }
+      if (i < b.size()){
+        result.add(b.get(i));
+      }
+    }
+    return result;
+  }
+  public static void main(String[]args){
+    SuperArray words = new SuperArray();
     //grouped to save vertical space
     words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
     words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
@@ -31,7 +43,6 @@ public class Demo{
     System.out.println(words);
     removeDuplicates(words);
     System.out.println(words);
-    */
 
     SuperArray numbers1 = new SuperArray();
     SuperArray numbers2 = new SuperArray();
@@ -44,5 +55,4 @@ public class Demo{
     System.out.println("input b " + numbers2);
     System.out.println("result " + findOverlap(numbers1, numbers2));
   }
-    /**/
 }
